c43336ea2f61691eb430a12afcbfe94e78324b71
johnniesimpson <johnniesimpson@outlook.com>
2021-11-02 19:14:30 -0400

    Clean up filter function and simplify the return.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,22 +12,28 @@ const Expenses = (props) => {
     console.log(filterData);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onFilterData={saveFilter} />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
